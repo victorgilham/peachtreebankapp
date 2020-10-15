ddf6d1dcc6eb9fee6f3bee569a2abf9db737b4e7
Victor Gil <victor.gil@agilethought.com>
2020-10-15 16:53:39 -0500

    add transaction mock items

--- src/app/shared/transaction/transaction-item/transaction-item.component.ts
+++ src/app/shared/transaction/transaction-item/transaction-item.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { ITransaction } from './_intefaces/transaction.interface';
 
 @Component({
   selector: 'app-transaction-item',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./transaction-item.component.scss']
 })
 export class TransactionItemComponent implements OnInit {
+  @Input() transactionItem: ITransaction;
 
   constructor() { }
 
